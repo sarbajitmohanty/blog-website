e39a188715ee611d095fa7faa20cb23da36d443b
sarbajitmohanty <sarbajit.mohanty02@gmail.com>
2021-05-18 17:50:52 +0530

    add compose page

--- app.js
+++ app.js
@@ -12,21 +12,25 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
-app.get("/", function(req, res){
-  res.render("home",{startingContent: homeStartingContent});
+app.get("/", function (req, res) {
+  res.render("home", { startingContent: homeStartingContent });
 });
 
 
-app.get("/about", function(req, res){
-  res.render("about",{about: aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contact: contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
 });
 
 
@@ -39,6 +43,6 @@ app.get("/contact", function(req, res){
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
